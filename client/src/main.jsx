d4c1b8294694ff519a5a36e4659dd435f266064f
New <n.phanuwit@gmail.com>
2024-02-10 04:15:06 +0700

    Menu

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Menu from './component/Menu.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Menu />
   </React.StrictMode>,
 )
