ca8f1cf6dd56b09871abeaa4ad4faed30b3a27de
Parunyu <zen.parunyu@gmail.com>
2024-02-26 22:09:39 +0700

    make own detect

--- client/src/component/utilities.jsx
+++ client/src/component/utilities.jsx
@@ -1,8 +1,8 @@
-export function drawRect(faces, ctx, trackedPersons) {
+export function drawRect(faces, ctx) {
   // Ensure faces is an array before attempting to iterate
   if (!Array.isArray(faces)) {
-      console.error("Invalid input. Expected an array.");
-      return;
+    console.error("Invalid input. Expected an array.");
+    return;
   }
   // Set styling
   ctx.strokeStyle = "white";
@@ -11,10 +11,10 @@ export function drawRect(faces, ctx, trackedPersons) {
   ctx.fillStyle = "white";
 
   // Loop through each detected face
-  faces.forEach((face, index) => {
-      // Extract box coordinates
-      const { xMin, yMin, width, height } = face.box;
-
+  faces.forEach((face) => {
+    // Extract box coordinates
+    // const { xMin, yMin, width, height } = face.box;
+    const { x, y, w, h } = face.facial_area;
     // Set styling
     ctx.strokeStyle = "rgba(255, 255, 255, 0.743)";
     ctx.lineWidth = 5;
@@ -23,14 +23,14 @@ export function drawRect(faces, ctx, trackedPersons) {
     // ctx.roundRect(10,20,80,80,[30]);
     ctx.beginPath();
     // ctx.rect(xMin, yMin, width, height);
-    ctx.roundRect(xMin, yMin, width, height,[7]);
+    ctx.roundRect(x, y, w, h, [7]);
     ctx.stroke();
-    
+
     // Draw ID for the corresponding tracked person
     // const trackedPerson = trackedPersons[index];
     // if (trackedPerson) {
     //     ctx.fillText("ID: " + trackedPerson.id, xMin, yMin - 10);
     // }
-    
-});
+
+  });
 };
